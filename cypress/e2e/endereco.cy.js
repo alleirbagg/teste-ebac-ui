6035a18647d7d98bc4b9edfe4fa025cba71751e4
Gabriella <gabgnoatto@gmail.com>
2023-09-07 20:12:39 -0300

    massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,7 @@
 ///<reference types="cypress" />
+import { defaultsDeep } from 'cypress/types/lodash';
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -6,11 +9,28 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         cy.fixture('perfil').then (dados =>{
             cy.login(dados.usuario, dados.senha)
         })
-        
-        
+                
     });
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+        EnderecoPage.editarEnderecoFaturamento('Gabriella', 'Gnoatto', 'EBAC', 'Brasil', 'Rua zero limonada', '00', 'Chapecó', 'Santa Catarina', '00000-000', '49999999999', 'gab@ebac.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     });
 });
